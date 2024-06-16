3f0db92d8b86b2147f64e8085e759c383e5aac82
dev.obeng.bismark@gmail.com <dev.obeng.bismark@gmail.com>
2024-06-16 11:24:55 +0000

    added realtor form

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,6 @@
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
+import { getSessionUser } from '@/utils/getSessionUser';
 
 export const GET = async (request) => {
 	try {
@@ -16,12 +17,63 @@ export const GET = async (request) => {
 	}
 };
 
-export const POST = async () => {
+export const POST = async (request) => {
 	try {
-		return new Response(JSON.stringify({ message: 'Sucess' }), {
-			status: 2000,
-		});
+		await connectDB();
+		const sessionUser = await getSessionUser();
+		if (!sessionUser || !sessionUser.userId) {
+			return new Response('User ID IS required', {
+				status: 401,
+			});
+		}
+		const { userId } = sessionUser;
+		const formData = await request.formData();
+
+		// Access all values from amenities and images
+		const amenities = formData.getAll('amenities');
+		const images = formData
+			.getAll('images')
+			.filter((image) => image.name !== '');
+		//create PropertyData object for database
+		const propertyData = {
+			type: formData.get('type'),
+			name: formData.get('name'),
+			description: formData.get('description'),
+			location: {
+				street: formData.get('location.street'),
+				city: formData.get('location.city'),
+				state: formData.get('location.state'),
+				zipcode: formData.get('location.zipcode'),
+			},
+			beds: formData.get('beds'),
+			baths: formData.get('baths'),
+			square_feet: formData.get('square_feet'),
+			amenities,
+			rates: {
+				weekly: formData.get('rates.weekly'),
+				monthly: formData.get('rates.monthly'),
+				nightly: formData.get('rates.nightly'),
+			},
+			seller_info: {
+				name: formData.get('seller_info.name'),
+				email: formData.get('seller_info.email'),
+				phone: formData.get('seller_info.phone'),
+			},
+			owner: userId,
+			//images,
+		};
+		const newPrperty = new Property(propertyData);
+		await newPrperty.save();
+
+		return Response.redirect(
+			`${process.env.NEXTAUTH_URL}/properties/${newPrperty._id}`
+		);
+		// console.log(propertyData);
+		// return new Response(JSON.stringify({ message: 'Sucess' }), {
+		// 	status: 200,
+		// });
 	} catch (error) {
+		console.log(error.message);
 		return new Response('Failed to add property');
 	}
 };
