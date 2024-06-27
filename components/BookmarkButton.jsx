d59ac0ce9a81a68622aede8c822f6175fcabb6a1
dev.obeng.bismark@gmail.com <dev.obeng.bismark@gmail.com>
2024-06-27 03:00:07 +0000

    add to remove from wishlist

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,6 +1,6 @@
 'use client';
 import { useSession } from 'next-auth/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FaBookmark } from 'react-icons/fa';
 import { toast } from 'react-toastify';
 
@@ -8,6 +8,27 @@ const BookmarkButton = ({ property }) => {
 	const { data: session } = useSession();
 	const userId = session?.user?.id;
 	const [isBookmarked, setIsBookmark] = useState();
+	useEffect(() => {
+		const checkBookmarkStatus = async () => {
+			try {
+				const res = await fetch('/api/bookmarks/check', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({ propertyId: property._id }),
+				});
+				if (res.status === 200) {
+					const data = await res.json();
+
+					setIsBookmark(data.isBookmarked);
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		checkBookmarkStatus();
+	}, [property._id, userId]);
 	const handleClick = async () => {
 		if (!userId) {
 			toast.error('You need to sign in to add it to your wish list');
@@ -31,7 +52,13 @@ const BookmarkButton = ({ property }) => {
 			toast.error('Something went wrong');
 		}
 	};
-	return (
+	return isBookmarked ? (
+		<button
+			onClick={handleClick}
+			className='bg-red-500 hover:bg-red-500 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
+			<FaBookmark className='mr-2' /> Remove Bookmark Property
+		</button>
+	) : (
 		<button
 			onClick={handleClick}
 			className='bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
